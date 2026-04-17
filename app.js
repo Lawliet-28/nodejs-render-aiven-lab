792901dfed76684abbdfccd4b8738fe9b741d4a0
Linuel Leogan <linuelleogan7@gmail.com>
2026-04-16 21:19:37 -0700

    commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
